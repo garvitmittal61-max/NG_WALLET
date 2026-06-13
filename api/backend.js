f8b9bb7d76eade4cf05ea26535441a75c92a596e
garvitmittal61-max <garvitmittal61@gmail.com>
2026-06-13 18:46:40 +0530

    Change bot token in backend.js
    Updated the bot token for authentication.

--- api/backend.js
+++ api/backend.js
@@ -16,7 +16,7 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 // 2. Updated Bot Token
-const BOT_TOKEN = "8949928597:AAE_tqWVa6dzmEQOg2QCrkYPUguSPNjiZiE";
+const BOT_TOKEN = "8949928597:AAEwJXfEzzLKs_YlBvLIt3Gowk3HLL1fsL4";
 
 function getExactDate() {
     return new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata', day: '2-digit', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
